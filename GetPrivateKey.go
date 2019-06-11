f75cb35c549a00a863cc1c96e709c91f488e1742
bipMaker <AMSafonov@gmail.com>
2019-06-11 21:54:45 +0300

    Первый коммит

---
+++ GetPrivateKey.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"bytes"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+	"github.com/miguelmota/go-ethereum-hdwallet"
+)
+
+var (
+	conf Config
+)
+
+type Config struct {
+	Mnemo      []string `toml:"mnemo"`
+	PublicKeys []string `toml:"public_key"`
+}
+
+func main() {
+	var (
+		pk     []string
+		buffer bytes.Buffer
+	)
+	ConfFileName := "getprivatekey.toml"
+	if _, err := toml.DecodeFile(ConfFileName, &conf); err != nil {
+		fmt.Println("Ошибка загрузки файла конфигурации:", err.Error())
+		return
+	}
+	for _, mn := range conf.Mnemo {
+
+		wallet, err := hdwallet.NewFromMnemonic(mn)
+		if err != nil {
+			panic(err)
+		}
+		path := hdwallet.MustParseDerivationPath("m/44'/60'/0'/0/0")
+		account, err := wallet.Derive(path, false)
+		if err != nil {
+			panic(err)
+		}
+
+		strAdrs := account.Address.String()
+		addrss := fmt.Sprintf("M%s", strings.ToLower(strAdrs[1:len(strAdrs)]))
+		fmt.Println(addrss)
+		privKeyStr, err := wallet.PrivateKeyHex(account)
+		if err != nil {
+			panic(err)
+		}
+		pk = append(pk, privKeyStr)
+
+	}
+	conf.PublicKeys = pk
+
+	encoder := toml.NewEncoder(&buffer)
+	if err := encoder.Encode(conf); err != nil {
+		fmt.Errorf("Ошибка сохранения файла: %s", err.Error())
+		return
+	}
+	ioutil.WriteFile(ConfFileName, []byte(buffer.String()), 0644)
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GetPrivateKey
 Утилита генерации приватных ключей по сид фразе для сети Минтер. Работает оффлайн
+
+Для работы необходим файл  getprivatekey.toml. В файле необходимо заполнить спиcок сид фраз для определения приватных ключей:
+mnemo = ["cruise flip live fatigue give staff target odor absent curtain absurd silly", "cover identify solve motor measure elephant shift lab recycle angle pulp wise", "final sheriff already hurt summer favorite hurry bread frozen guide garden ahead"]
+
+После работы программы в этом файле появится список публичных ключей
+public_key = ["111a72c27420c42db156246d45d39b75ad67134486995eee98c8dc900453caec", "b2dfd08437e00b447bd063ab144da254ccaa2c98f2170b2d1ec9d806b17d6737", "644abc5e86ed56214d55706e5c41b3ad10358c320b2faa033871069160e770a0"]
+
+Благодарности можно выслать на адрес: Mx32f7b28dd727c0a2017ba4c9dec7257007876495 :)
